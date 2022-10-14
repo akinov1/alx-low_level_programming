d592dc5eebfdaebe2661cda6abf729a94dab30ec
akinov1 <akinakinteyee@gmail.com>
2022-10-14 16:39:48 -0700

    prints

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,21 +5,21 @@
 */
 int main(void)
 {
-int num;
+	int num;
 
 	for (num = '0'; num <= '9'; num++)
-{
-	putchar(num);
-	if (num == '9')
 	{
-																	break;
+		putchar(num);
+		if (num == '9')
+		{
+		break;
+		}
+		else
+		{
+		putchar(',');
+		putchar(' ');
+		}
 	}
-	else
-	{
-	putchar(',');
-																	putchar(' ');
-	}
-}
 	putchar('\n');
 	return (0);
 }
