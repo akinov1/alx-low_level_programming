f1baf0aee8908564e41215bb8c2b9c455dafd1d5
akinov1 <akinov1@gmail.com>
2022-10-27 00:05:04 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,6 @@
  * @src: input value
  *
  * Return: void
- *
 */
 
 char *_strcat(char *dest, char *src)
@@ -16,7 +15,9 @@ char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
+
 	i = 0;
+
 	while (dest[i] != '\0')
 
 	{
